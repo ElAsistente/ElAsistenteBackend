19fa57dd73c0f08b8b93a3b29129774c9c72cacd
Javier <carlosjj89@gmail.com>
2016-06-27 17:03:33 -0300

    Script para poblar la db
    Poner True en el if para poblar

---
+++ models/10_poblar.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from gluon.contrib.populate import populate
+
+if False:
+    populate(db.turno, 3)
+    populate(db.modulo, 14)
+    populate(db.alumno, 100)
+    populate(db.profesor, 20)
+    populate(db.materia, 80)
+    populate(db.comision, 120)
+    populate(db.materia_comision_ciclo, 100)
+    populate(db.persona_materia_comision, 100)
+    populate(db.asistencia, 120)
+    populate(db.aula, 30)
+    populate(db.examen_final, 100)
